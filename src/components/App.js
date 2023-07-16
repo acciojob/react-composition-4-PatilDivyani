3bfe59c5957baad4aa495084740fd28e310baa4b
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-07-16 12:27:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+   let [user,setUser] = useState({name:'',email:'',password:''})
+  function submitFunc(){
+    console.log(user);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form onSubmit={submitFunc}>
+        
+        <label>Name</label>
+        <input type='text' placeholder='Name' 
+        onChange={(e)=>setUser({...user, name: e.target.value})} />
+        
+        <label>Email</label>
+        <input type='email' placeholder='Email' 
+        onChange={(e)=>setUser({...user, email: e.target.value})} />
+        
+        <label>Password</label>
+        <input type='password' placeholder='Password' 
+        onChange={(e)=>setUser({...user, password: e.target.value})} />
+        <button>Submit</button>
+        </form>
     </div>
   )
 }
